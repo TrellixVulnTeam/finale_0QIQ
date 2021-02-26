e1383b07fc430b873f122dda8313f85f49e4b469
vitaliiosadets <shgrtwb@gmail.com>
2021-02-26 20:32:54 +0200

    exam without mobile version

---
+++ osadets_vitalii/src/js/feedbacksSlider.js
@@ -0,0 +1,25 @@
+import $ from "jquery";
+import "slick-carousel";
+
+$(document).ready(function () {
+    $(".feedbacks__slider").slick({
+        arrows: true,
+        dots: false,
+        infinite: true,
+        speed: 1000,
+        autoplaySpeed: 200000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                },
+            },
+        ],
+    });
+});
---
+++ osadets_vitalii/src/js/script.js
@@ -0,0 +1,6 @@
+import tab from "./tab";
+import accordion from "./accordion";
+import addToCart from "./addToCart";
+import opencart from "./openCart";
+import slider from "./feedbacksSlider";
+import headerScroll from "./headerScroll";
